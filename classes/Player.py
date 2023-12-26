86f5780289450fabf53bddea75c83bf97440321b
Lucca <luccajbecker@gmail.com>
2023-12-26 00:30:44 -0300

    sintax

--- classes/Player.py
+++ classes/Player.py
@@ -1,19 +1,17 @@
 from .RiotAPI import RiotAPI
 
-riot_requests = RiotAPI('RGAPI-0cadcd4f-a1c3-4af1-bc93-c9a0fba9aec1')
+riotRequests = RiotAPI('RGAPI-0cadcd4f-a1c3-4af1-bc93-c9a0fba9aec1')
 
 class Player():
-  
-  riot_requests = RiotAPI('RGAPI-0cadcd4f-a1c3-4af1-bc93-c9a0fba9aec1')
 
   def __init__(self, nameAndTag):
     splited = nameAndTag.split('#')
 
     self.name = splited[0] #name
     self.tag = splited[1] #tag
-    self.puuid = riot_requests.summoner_request(self.name, self.tag)
-    self.encryptedSummonerId = riot_requests.encryptedSummonerId_request(self.puuid)
-    self.summoner = riot_requests.playerStatus(self.encryptedSummonerId)
+    self.puuid = riotRequests.summonerRequest(self.name, self.tag)
+    self.encryptedSummonerId = riotRequests.encryptedSummonerIdRequest(self.puuid)
+    self.summoner = riotRequests.playerStatus(self.encryptedSummonerId)
 
   def infos(self):
     print([self.name, self.tag, self.puuid, self.encryptedSummonerId, self.summoner])
--- classes/RiotAPI.py
+++ classes/RiotAPI.py
@@ -1,22 +1,22 @@
 import requests
 
 class RiotAPI():
-  def __init__(self, api_key):
-    self.api_key = api_key
-    self.summoner_url = 'https://americas.api.riotgames.com/riot/account/v1/accounts/by-riot-id/'
-    self.encryptedSummonerId_url = 'https://br1.api.riotgames.com/lol/summoner/v4/summoners/by-puuid/'
-    self.summoner_stats = 'https://br1.api.riotgames.com/lol/league/v4/entries/by-summoner/'
+  def __init__(self, apiKey):
+    self.apiKey = apiKey
+    self.summonerUrl = 'https://americas.api.riotgames.com/riot/account/v1/accounts/by-riot-id/'
+    self.encryptedSummonerIdUrl = 'https://br1.api.riotgames.com/lol/summoner/v4/summoners/by-puuid/'
+    self.summonerStats = 'https://br1.api.riotgames.com/lol/league/v4/entries/by-summoner/'
   
-  def summoner_request(self, name, tag):
-    summoner_request = f'{self.summoner_url}{name}/{tag}?api_key={self.api_key}'
-    return requests.get(summoner_request).json()['puuid']
+  def summonerRequest(self, name, tag):
+    summonerRequest = f'{self.summonerUrl}{name}/{tag}?api_key={self.apiKey}'
+    return requests.get(summonerRequest).json()['puuid']
 
-  def encryptedSummonerId_request(self, puuid):
-    encryptedSummonerId = f'{self.encryptedSummonerId_url}{puuid}?api_key={self.api_key}'
+  def encryptedSummonerIdRequest(self, puuid):
+    encryptedSummonerId = f'{self.encryptedSummonerIdUrl}{puuid}?api_key={self.apiKey}'
     return requests.get(encryptedSummonerId).json()['id']
   
   def playerStatus(self, encryptedSummonerId):
-    summoner_sts = f'{self.summoner_stats}{encryptedSummonerId}?api_key={self.api_key}'
-    return requests.get(summoner_sts).json()
+    summonerSts = f'{self.summonerStats}{encryptedSummonerId}?api_key={self.apiKey}'
+    return requests.get(summonerSts).json()
 
 
